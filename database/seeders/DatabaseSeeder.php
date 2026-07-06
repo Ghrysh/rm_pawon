3ca700a8b6f9f1b455e6af8f84c61f12850efe7a
Ghrysh <gispi.desu@gmail.com>
2026-07-07 04:29:32 +0700

    seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,39 +19,5 @@ class DatabaseSeeder extends Seeder
             'username' => 'admin',
             'password' => bcrypt('password'),
         ]);
-
-        $makanan = \App\Models\Category::create(['name' => 'Makanan']);
-        $minuman = \App\Models\Category::create(['name' => 'Minuman']);
-        $cemilan = \App\Models\Category::create(['name' => 'Cemilan']);
-        $paket = \App\Models\Category::create(['name' => 'Paket Liwet']);
-        $coffee = \App\Models\Category::create(['name' => 'Coffee']);
-
-        // Insert some menus for Paket Liwet
-        for ($i = 1; $i <= 5; $i++) {
-            \App\Models\Menu::create([
-                'category_id' => $paket->id,
-                'name' => 'Paket Liwet ' . $i,
-                'description' => 'Lorem Ipsum has been the industry\'s standard dummy text ever since the',
-                'price' => 45000,
-                'stok' => 10,
-                'image' => '/assets/bg_mobile.png' // using placeholder
-            ]);
-        }
-
-        // Insert some other menus
-        \App\Models\Menu::create([
-            'category_id' => $makanan->id,
-            'name' => 'Nasi Goreng Spesial',
-            'description' => 'Nasi goreng dengan telur dan ayam.',
-            'price' => 25000,
-            'stok' => 5,
-        ]);
-        \App\Models\Menu::create([
-            'category_id' => $makanan->id,
-            'name' => 'Ayam Bakar Madu',
-            'description' => 'Ayam bakar manis gurih dengan madu pilihan.',
-            'price' => 30000,
-            'stok' => 12,
-        ]);
     }
 }
